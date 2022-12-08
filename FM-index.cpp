6a6d3fd47b93736bff450ec283e68cb6f737bc40
Diego Iturra <diturra@dparadig.com>
2022-12-08 12:46:54 -0300

    update: se cambia indentacion de codigo fm index

--- FM-index.cpp
+++ FM-index.cpp
@@ -14,21 +14,21 @@ using namespace std;
 
 int main(int argc, char** argv) {
     if (argc !=  2) {
-    cout << "Uso: " << argv[0] << " <archivo entrada>" << endl;
-    return 1;
-  }
+        cout << "Uso: " << argv[0] << " <archivo entrada>" << endl;
+        return 1;
+    }
 
-  // Una versión compacta del suffix array, implementada en la biblioteca
-  // SDSL. En este caso, utiliza un wavelet tree tipo wt_int<> como
-  // building-block
-  // Otros posibles building blocks son: wt_huff<>, wt_int<rrr_vector<> >, entre
-  // otras combinaciones
-  cout << "Construyendo el FM-index ..." << endl;
-  csa_wt<wt_int<>> fm_index;
-  construct(fm_index, argv[1], 1);
-  
+    // Una versión compacta del suffix array, implementada en la biblioteca
+    // SDSL. En este caso, utiliza un wavelet tree tipo wt_int<> como
+    // building-block
+    // Otros posibles building blocks son: wt_huff<>, wt_int<rrr_vector<> >, entre
+    // otras combinaciones
+    cout << "Construyendo el FM-index ..." << endl;
+    csa_wt<wt_int<>> fm_index;
+    construct(fm_index, argv[1], 1);
+      
     
-  cout << "Tamaño del FM-index " << size_in_mega_bytes(fm_index) << " MB." << endl;
+    cout << "Tamaño del FM-index " << size_in_mega_bytes(fm_index) << " MB." << endl;
 
     string patron;
     cout << "Ingrese un patrón a buscar: ";
@@ -42,14 +42,14 @@ int main(int argc, char** argv) {
 
     cout << "# de ocurrencias: " << occs << endl;
     if (occs > 0) {
-      cout << "Las ocurrencias comienzan en las siguientes posiciones: " << endl;
-      //      auto locations = locate(fm_index, query.begin(), query.begin()+m)
-      auto posiciones = sdsl::locate(fm_index, patron.begin(), patron.end());
-      sort(posiciones.begin(), posiciones.end());
+        cout << "Las ocurrencias comienzan en las siguientes posiciones: " << endl;
+        //auto locations = locate(fm_index, query.begin(), query.begin()+m)
+        auto posiciones = sdsl::locate(fm_index, patron.begin(), patron.end());
+        sort(posiciones.begin(), posiciones.end());
       
-      for (size_t i = 0; i < occs; ++i) {
-	cout << posiciones[i] << endl;
-      }
+        for (size_t i = 0; i < occs; ++i) {
+	       cout << posiciones[i] << endl;
+        }
     }
     return 0;
 }
